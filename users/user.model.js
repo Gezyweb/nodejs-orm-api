f8da92c05a30a5766034dc2915350cf19d6f45a7
Gezyweb <gezywebibjong@gmail.com>
2024-03-22 06:19:52 +0800

    initial

--- users/user.model.js
+++ users/user.model.js
@@ -1,24 +1,27 @@
-const {DataTypes} = require('sequelize');
+const {DataTypes}=require('sequelize');
 
-module.exports = model;
+module.exports=model;
 
 function model(sequelize){
-    const attributes = {
-        email: {type: DataTypes.STRING, allowNull:false},
-        passwordHash: {type: DataTypes.STRING, allowNull:false},
-        title:{type: DataTypes.STRING,allowNull: false},
-        firstName:{type: DataTypes.STRING,allowNull: false},
-        lastName:{type: DataTypes.STRING,allowNull: false},
-        role:{type: DataTypes.STRING,allowNull: false},
+    const attributes ={
+        email: {type: DataTypes.STRING, allowNull: false},
+        passwordHash:{type: DataTypes.STRING, allowNull: false},
+        title:{type: DataTypes.STRING, allowNull: false},
+        firstName:{type: DataTypes.STRING, allowNull: false},
+        lastName:{type: DataTypes.STRING, allowNull: false},
+        role:{type: DataTypes.STRING, allowNull: false}
     };
 
-    const options = {
-        defaultScope:{
-            attributes: {exclude:['passwordHash']}
+    const options ={
+        defaultScope: {
+            //exclude password hash by defaultl
+            attributes: {exclude: ['passwordHash'] }
         },
         scopes:{
-            withHash:{attributes:{},}
+            //include hash with this scope
+            withHash: { attributes: {}, }
         }
     };
-    return sequelize.define('User',attributes,options);
+
+    return sequelize.define('User', attributes, options);
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -9,65 +9,67 @@ const userService = require('./user.service');
 
 router.get('/', getAll);
 router.get('/:id', getById);
-router.get('/', createSchema, create);
-router.get('/:id', updateSchema, update);
-router.get('/:id', _delete);
+router.post('/', createSchema, create);
+router.put('/:id', updateSchema, update);
+router.delete('/:id', _delete);
 
-module.exports  = router;
+module.exports = router;
 
 //route functions
 
-function getAll(req, res, nest){
+function getAll(req, res, next){
     userService.getAll()
         .then(users => res.json(users))
         .catch(next);
 }
 
-function getById(req, res, next){
+function getById(req,res,next){
     userService.getById(req.params.id)
         .then(user => res.json(user))
         .catch(next);
 }
 
-function create(req, res, next){
+function create(req,res,next){
     userService.create(req.body)
-        .then(() => res.json({message: 'User Created'}))
+        .then(() => res.json({ message: 'User created'}))
         .catch(next);
 }
 
-function update(req, res, next){
-    userService.delete(req.params.id)
-        .then(() => res.json({message: 'User Updated'}))
+function update(req,res,next){
+    userService.update(req.params.id,req.body)
+        .then(() => res.json({message: 'User updated'}))
+        .catch(next);
 }
 
-function _delete(res, req, next){
+function _delete(req,res,next){
     userService.delete(req.params.id)
-        .then(() => res.json({ message: 'User deleted'}))
+        .then(() => res.json({message: 'User deleted'}))
+        .catch(next);
 }
+// schema functions
 
-function createSchema(req, res, next){
+function createSchema(req,res,next){
     const schema = Joi.object({
         title: Joi.string().required(),
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        role: Joi.string().valid(Role.Admin, Role.User).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min().required(),
+        role: Joi.string().valid(Role.Admin,Role.User).required(),
+        email: Joi.string().required(),
+        password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required()
     });
-    validateRequest(req, res, schema);
+    validateRequest(req, next, schema);
 }
 
-function updateSchema(req, res, next){
+function updateSchema(req,res,next){
     const schema = Joi.object({
         title: Joi.string().empty(''),
         firstName: Joi.string().empty(''),
         lastName: Joi.string().empty(''),
         role: Joi.string().valid(Role.Admin, Role.User).empty(''),
-        title: Joi.string().email().empty(''),
-        title: Joi.string().min(6).empty(''),
-        title: Joi.string().valid(Joi.ref('password')).empty('')
-
-    }).wth('password', 'confirmpassword');
-    validateRequest(req, next, schema);
+        email: Joi.string().empty(''),
+        password: Joi.string().min(6).empty(''),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
+    }).with('password','confirmPassword');
+    validateRequest(req,next,schema);
 }
