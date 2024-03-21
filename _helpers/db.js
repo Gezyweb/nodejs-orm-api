0d43e8cc4e2757cf6ee92847a81462ce44fa2778
Gezyweb <gezywebibjong@gmail.com>
2024-03-22 06:36:02 +0800

    Update code

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,19 +1,21 @@
 const config = require('config.json');
 const mysql = require('mysql2/promise');
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
 
-initialize();
-async function initialize() {
-    const {host, port, user, password, database} = config.database;
-    const connection = await mysql.createConnection({host, port, user, password});
-    await connection.query(`CREATE DATABASE IF NOT EXIST \`${database}\`;`);
+initialize()
+
+async function initialize(){
+    
+    const { host, port ,user ,password, database} = config.database;
+    const connection = await mysql.createConnection({host,port,user,password})
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     const sequelize = new Sequelize(database, user, password, {dialect: 'mysql'});
 
-    db.User = require('../users/user.model')(sequelize);
+    db.User = require('../users/user.model')(sequelize)
 
-    await sequelize.sync({alter: true});
-    
+    await sequelize.sync({alter : true});
+
 }
